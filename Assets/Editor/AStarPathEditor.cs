d37ab7302df13e3d1ec4c5649eefe901b9f1a925
chenlinyan <874499648@qq.com>
2019-02-02 10:23:02 +0800

    #提交网格生成器项目

---
+++ Assets/Editor/AStarPathEditor.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using KBEngine;
+
+
+[CustomEditor(typeof(AStarPath))]
+public class AStarPathEditor : Editor
+{
+    public Dictionary<int, GraphicEditorBase> graphEditors = new Dictionary<int, GraphicEditorBase>();
+    static List<string> layersName = new List<string>();
+
+    AStarPath aStarPathScript;
+    public override void OnInspectorGUI()
+    {
+        int editorNum = graphEditors.Count;
+        int graphNum = aStarPathScript.Graphs.Count;
+
+        if (editorNum == 0 && graphNum == 0)
+        {
+            AddGraph(AStarData.GraphType.GridGraph);
+            graphNum = 1;
+        }
+
+        if (editorNum < graphNum)
+        {
+            for (int i = editorNum; i < graphNum; i++)
+            {
+                var graphObj = aStarPathScript.Graphs[i];
+                CreateGraphEditor(graphObj.graphType, graphObj);
+            }
+        }
+
+        Dictionary<int, GraphicEditorBase>.KeyCollection keyCol = graphEditors.Keys;
+        foreach(int graphIndex in keyCol)
+        { 
+            if (graphEditors[graphIndex] == null)
+            {
+                Debug.Log("graphEditors graphIndex[" + graphIndex + "] is null!" );
+                continue;
+            }
+            graphEditors[graphIndex].DrawInspectorGUI();
+        }
+
+        bool flag = aStarPathScript.ShowGraphs;
+        aStarPathScript.ShowGraphs = EditorGUILayout.Toggle("Show Graphs", aStarPathScript.ShowGraphs);
+        if (flag != aStarPathScript.ShowGraphs)
+        {
+            if (!Application.isPlaying || EditorApplication.isPaused) SceneView.RepaintAll();
+        }
+
+        if (GUILayout.Button(new GUIContent("Scan", "Recaculate all graphs")))
+        {
+            for(int i = 0; i < graphNum; i++)
+            {
+                if (aStarPathScript.Graphs[i] == null)
+                {
+                    Debug.Log("aStarPathScript.Graphs[" + i + "] is null!");
+                    continue;
+                }
+                aStarPathScript.Graphs[i].ScanGraphicInternal();
+            }
+        }
+
+        if (GUILayout.Button(new GUIContent("Export", "Export graph data")))
+        {
+            for (int i = 0; i < graphNum; i++)
+            {
+                if (aStarPathScript.Graphs[i] == null)
+                {
+                    Debug.Log("Export_aStarPathScript.Graphs[" + i + "] is null!");
+                    continue;
+                }
+                aStarPathScript.Graphs[i].ExportGraphData();
+            }
+        }
+
+    }
+
+    public void OnEnable()
+    {
+        aStarPathScript = target as AStarPath;
+        aStarPathScript.OnDrawGizmosCallback = OnDrawGizmos;
+    }
+
+    public void AddGraph(AStarData.GraphType type)
+    {
+        aStarPathScript.AddGraph(type);
+    }
+
+    private void CreateGraphEditor(int type, GraphBase graphObj)
+    {
+        CreateGraphEditor((AStarData.GraphType)(type), graphObj);
+    }
+
+    private void CreateGraphEditor(AStarData.GraphType type, GraphBase graphObj)
+    {
+        switch (type)
+        {
+            case AStarData.GraphType.GridGraph:
+                {
+                    GridGraphEditor gridGraph = new GridGraphEditor();
+                    gridGraph.targetGraph = graphObj;
+                    graphEditors.Add(graphObj.graphIndex, gridGraph);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        int graphNum = aStarPathScript.Graphs.Count;
+        for (int i = 0; i < graphNum; i++)
+        {
+            var graphObj = aStarPathScript.Graphs[i];
+            graphObj.OnDrawGizmos(true);
+        }
+    }
+
+
+    public static string[] GetLayerMaskField(bool refreshFlag = false)
+    {
+        if (!refreshFlag && layersName.Count != 0) return layersName.ToArray();
+        if (layersName.Count != 0)
+        {
+            layersName.Clear();
+        }
+
+        int emptyLayers = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            string layerName = LayerMask.LayerToName(i);
+
+            if (layerName != "")
+            {
+
+                for (; emptyLayers > 0; emptyLayers--) layersName.Add("Layer " + (i - emptyLayers));
+                layersName.Add(layerName);
+            }
+            else
+            {
+                emptyLayers++;
+            }
+        }
+
+        return layersName.ToArray();
+    }
+}
+
---
+++ Assets/Script/GridGenerator/Core/AStar/AStarPath.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using KBEngine;
+
+namespace KBEngine
+{
+    public class AStarPath : MonoBehaviour
+    {
+        public System.Action OnDrawGizmosCallback;
+        public List<GraphBase> Graphs
+        {
+            get
+            {
+                return AStarData.graphs;
+            }
+            set
+            {
+                AStarData.graphs.Clear();
+                for (int i = 0; i < value.Count; i++)
+                {
+                    value[i].graphIndex = AStarData.graphIndex++;
+                }
+                AStarData.graphs = value;
+
+            }
+        }
+
+        public bool ShowGraphs
+        {
+            get
+            {
+                return showGraphs;
+            }
+            set
+            {
+                showGraphs = value;
+            }
+        }
+
+        private bool showGraphs = false;
+        private void Awake()
+        {
+           // AddGraph(AStarData.GraphType.GridGraph);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void AddGraph(int type)
+        {
+            AStarData.GraphType graphType = (AStarData.GraphType)type;
+            AddGraph(graphType);
+        }
+
+        public void AddGraph(AStarData.GraphType graphType)
+        {
+            switch (graphType)
+            {
+                case AStarData.GraphType.GridGraph:
+                    {
+                        GridNavGraph gridGraph = new GridNavGraph(AStarData.graphIndex++);
+                        gridGraph.graphType = (int)graphType;
+                        AStarData.AddGrapData(gridGraph);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+         
+        private void OnDrawGizmos()
+        {
+            if (OnDrawGizmosCallback != null)
+            {
+                if (ShowGraphs)
+                {
+                    OnDrawGizmosCallback();
+                }
+                   
+            }
+        }
+
+
+    }
+
+}
